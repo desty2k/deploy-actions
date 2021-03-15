3a08cee7323024c7320dc3f1ce6b0f40486eb11d
desty2k <55806660+desty2k@users.noreply.github.com>
2021-03-15 18:41:26 +0100

    ADD: args parser

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,11 @@
+import sys
+import argparse
+from os.path import abspath, dirname
+
+
+sys.path.insert(0, abspath(dirname(abspath(__file__)) + '/..'))
+from app import __version__
+
 
 class Main:
     def __init__(self):
@@ -11,5 +19,13 @@ class Main:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="App")
+    parser.add_argument('-v', '--version', action='version',
+                        version='v{}'.format(__version__))
+
+    args = parser.parse_args()
+    if len(sys.argv) <= 1:
+        parser.print_help()
+
     main = Main()
     main.run()
